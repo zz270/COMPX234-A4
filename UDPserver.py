de7ffb561b12fc9ad7fcccfd03fdc67dee601c70
zz270 <3128388364@qq.com>
2025-06-14 12:51:31 +0000

    commit

--- UDPserver.py
+++ UDPserver.py
@@ -3,3 +3,9 @@ import threading
 import random
 import os
 import base64
+class UDPServer:
+    def __init__(self, port):
+        self.server_port = port
+        self.welcome_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.welcome_socket.bind(('', self.server_port))
+        print(f"Server started on port {self.server_port}")
